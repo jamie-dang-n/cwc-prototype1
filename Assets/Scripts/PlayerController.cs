2b2287f912e1e7b39579048adebaf1ccc35859e3
gothhouse <jamiedang1349@gmail.com>
2019-12-16 10:01:02 -0800

    Made forward and horizontal input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
     public float speed = 5.0f;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +18,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+
         //move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime  * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime  * forwardInput);
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput);
     }
 }
